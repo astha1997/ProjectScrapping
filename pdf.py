1050dfea29f2dfd2150d3c88dcacb0c47086ba41
astha1997 <76031639+astha1997@users.noreply.github.com>
2023-02-20 09:50:27 +0530

    Update pdf.py

--- pdf.py
+++ pdf.py
@@ -3,39 +3,30 @@ from Scrapper import all_course, scrap_all, get_course
 
 
 
-
-
-
-
-pdf = FPDF()
-pdf.add_page()
-# set style and size of font
-# that you want in the pdf
-pdf.set_font("Arial", size = 15)
-
-
-
 count=0
 list_courses = all_course()
 for i in list_courses:
         filename=i+'.pdf'
-        print(filename)
+        pdf = FPDF()
+        pdf.add_page()
         count+=1
         if filename=='Be A DevOps Pro Tech Neuron.pdf' or filename=='Salesforce Administrator.pdf' or filename=='Cyber Security Masters.pdf' or filename=='Tibco Business Works.pdf' or filename=='Cyber Security Foundations.pdf' or filename=='Explainable AI.pdf' or filename=='Be A DevOps Pro.pdf' or filename=='Youtube Mastery Course in Hindi Tech Neuron.pdf' or filename=='Cyber Security Masters Tech Neuron.pdf':
                 continue
-        
+        print(filename)
         CourseName=get_course(i)['Course_title']
         pdf.set_font("Arial", size = 11)
         pdf.cell(200, 10,  txt = 'CourseName',ln = 1, align = 'L')
         #pdf.set_font("Arial", size = 9)
         pdf.multi_cell( 200,10,txt = CourseName)
+        #print('CourseName' ,CourseName)
             
         Description=get_course(i)['Description']
         pdf.set_font("Arial", size = 11)
         pdf.cell(200, 10,  txt = 'Description',ln = 1, align = 'L')
         #pdf.set_font("Arial", size = 9)
         pdf.multi_cell( 200,10,txt = Description)
-            
+        #print('Description' ,Description)   
+
         Language=get_course(i)['Language']
         pdf.set_font("Arial", size = 11)
         pdf.cell(200, 10,  txt = 'Language',ln = 1, align = 'L')
@@ -74,14 +65,4 @@ for i in list_courses:
         if(count>=50):
                 break
     
-    
- 
-
-
- 
-
-
-                    
-    
-
     
